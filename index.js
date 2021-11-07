cdc5310d28fa0b8a566ddb42100081d916067426
Springhawk27 <sajjad0mahmud@gmail.com>
2021-11-08 01:08:19 +0600

    booking data added

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42wwv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -21,7 +22,30 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('database connceted successfully')
+        // console.log('database connceted successfully')
+        const database = client.db('doctors_portal');
+        const appointmentCollection = database.collection('appointments');
+
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentCollection.insertOne(appointment);
+            console.log(appointment);
+            console.log(result);
+            res.json(result)
+        })
+
+        // // all user
+        // app.get('/users')
+        // // post a single user
+        // app.post('/users')
+        // // specific user
+        // app.get('/users/:id')
+        // // update a specific user
+        // app.put('/users/:id')
+        // // delete a specific user
+        // app.delete('/users/:id')
+        // // users: get(all or single data)
+        // // users: post(single data)
 
     }
     finally {

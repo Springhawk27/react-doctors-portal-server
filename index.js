36ee3544361bdc66a092b5bbcd20c03d2f53a1e4
Springhawk27 <sajjad0mahmud@gmail.com>
2021-11-08 20:31:38 +0600

    appointments collection added for dashboard

--- index.js
+++ index.js
@@ -26,6 +26,21 @@ async function run() {
         const database = client.db('doctors_portal');
         const appointmentCollection = database.collection('appointments');
 
+        app.get('/appointments', async (req, res) => {
+            const email = req.query.email;
+            // const date = req.query.date.toLocaleDateString();
+            const date = new Date(req.query.date).toDateString();
+            // const query = { email: email };
+            const query = { email: email, date: date };
+
+            console.log(date)
+
+            console.log(query)
+            const cursor = appointmentCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        })
+
         app.post('/appointments', async (req, res) => {
             const appointment = req.body;
             const result = await appointmentCollection.insertOne(appointment);
